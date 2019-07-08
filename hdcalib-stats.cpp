36114feb8d336418a8b615a531c83906794f1c7a
Alexander <Brock.Alexander@web.de>
2019-07-08 16:17:25 +0200

    Enable parallel plotting

--- hdcalib-stats.cpp
+++ hdcalib-stats.cpp
@@ -234,7 +234,7 @@ void Calib::plotResidualsByMarkerStats(
 
 void Calib::plotReprojectionErrors(const string prefix, const string suffix) {
     MarkerMap residuals_by_marker;
-//#pragma omp parallel for schedule(dynamic)
+#pragma omp parallel for schedule(dynamic)
     for (size_t ii = 0; ii < imagePoints.size(); ++ii) {
         plotReprojectionErrors(ii, residuals_by_marker, prefix, suffix);
     }
